f110d62c36353797466dccecea0b4e333286ab96
eqzpert <rubenlarsson97@gmail.com>
2023-11-09 15:22:08 +0100

    linked layout to code and added title

--- app/src/main/java/com/example/labb1_martin/MainActivity.java
+++ app/src/main/java/com/example/labb1_martin/MainActivity.java
@@ -3,13 +3,30 @@ package com.example.labb1_martin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button refreshButton;
+    private TextView viewContainer;
+    private TextView temperatureView;
+    private TextView windView;
+    private TextView cloudView;
+    private TextView rainView;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        refreshButton = findViewById(R.id.refreshButton);
+        viewContainer = findViewById(R.id.weathercontainer);
+        temperatureView = findViewById(R.id.temperature);
+        windView = findViewById(R.id.windspeed);
+        cloudView = findViewById(R.id.cloudiness);
+        rainView = findViewById(R.id.rain);
+        imageView = findViewById(R.id.weatherImage);
     }
 }
 
